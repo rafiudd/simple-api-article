aaf6011a220937807a6324cc6f0f177fb8455d56
rafiudd <sirin6867@gmail.com>
2020-09-01 12:15:07 +0700

    refactor: change routing on index

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const basicAuth = require('express-basic-auth');
 const dotenv = require('dotenv');
 const errorHandler = require('helpers/error-handler');
 const jwt = require('helpers/jwt');
+const userController = require('./modules/users/users.controller')
+const articleController = require('./modules/article/article.controller');
 
 dotenv.config();
 app.use(jwt());
@@ -25,17 +27,24 @@ app.use(cors({origin: true, credentials: true}));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
 app.use(bodyParser.json({ limit: '50mb' }));
 
-app.get('/', useBasicAuth, function (req, res) {
+app.get('/', function (req, res) {
     res.json({
+        success: true,
         status: 200,
         message: 'Service is successfull running :D'
     });
 });
 
-app.use('/api/v1/users',useBasicAuth, require('./modules/users/users.controller'));
-app.use('/api/v1/article', require('./modules/article/article.controller'));
+app.post('/api/v1/users/login', useBasicAuth, userController.login);
+app.post('/api/v1/users/login', useBasicAuth, userController.register);
+app.put('/api/v1/users/update/:userId', userController.updateUser);
 
-// start server
+app.post('/api/v1/article/create', articleController.createArticle);
+app.get('/api/v1/article/all', articleController.getAllArticle);
+app.get('/api/v1/article/', articleController.getDetailArticle);
+app.delete('/api/v1/article/delete/:articleId', articleController.deleteArticle);
+app.put('/api/v1/article/update/:articleId', articleController.updateArticle);
+
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 8080;
 app.listen(port, function () {
     console.log('Server listening on port ' + port);
